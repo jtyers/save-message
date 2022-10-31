1d9a6a25ea2411c914aff96079bc9e686d2d5cb8
jtyers <40065647+jtyers@users.noreply.github.com>
2022-10-31 21:03:04 +0000

    feat: refactor save_message module to use classes and pinject

--- save_message/injector.py
+++ save_message/injector.py
@@ -1,14 +1,18 @@
 import logging
 import pinject
 
+from save_message.config import load_config
+from save_message.config import CONFIG_FILE
+
+
 logger = logging.getLogger(__name__)
 
 
 class SaveMessageBindingSpec(pinject.BindingSpec):
     def configure(self, bind):
-        pass
-        # bind(
-        #    "endpoint",
-        #    to_instance=get_aaas_api_endpoint(),
-        # )
+        bind(
+            "config",
+            to_instance=load_config(CONFIG_FILE),
+        )
+
         # bind("task_service", to_class=TaskServiceClient)
--- save_message/save.py
+++ save_message/save.py
@@ -31,107 +31,113 @@ def guess_ext_for_part(part):
     return ext
 
 
-def save_part(msg, part, message_name, dest_dir, counter):
-    # multipart/* are just containers
-    if part.get_content_maintype() == "multipart":
-        return
+class MessageSaver:
+    """Saves messages, optionally with some transformations, to a configured
+    destination."""
 
-    filename = part.get_filename()
-    if filename:
-        filename = sanitize_to_filename(filename)
+    def __init__(self, config):
+        self.config = config
 
-        if "." in filename:
-            ext = filename[filename.index(".")]
+    def save_part(self, msg, part, message_name, dest_dir, counter):
+        # multipart/* are just containers
+        if part.get_content_maintype() == "multipart":
+            return
+
+        filename = part.get_filename()
+        if filename:
+            filename = sanitize_to_filename(filename)
+
+            if "." in filename:
+                ext = filename[filename.index(".")]
+            else:
+                ext = guess_ext_for_part(part)
+                filename = filename + ext
+
         else:
             ext = guess_ext_for_part(part)
-            filename = filename + ext
+            filename = f"{message_name}-{counter:03d}{ext}"
 
-    else:
-        ext = guess_ext_for_part(part)
-        filename = f"{message_name}-{counter:03d}{ext}"
+        counter += 1
+        dest_filename = os.path.join(dest_dir, filename)
+        with open(dest_filename, "wb") as fp2:
+            # if this part is not an attachment, it is the body of the message, so
+            # we prepend some headers to give context
+            if not part.is_attachment() and part.get_content_maintype() == "text":
+                for h in ["Date", "From", "To", "Subject"]:
+                    fp2.write(f"{h}: {msg[h.lower()]}\n".encode())
+                fp2.write("\n".encode())
 
-    counter += 1
-    dest_filename = os.path.join(dest_dir, filename)
-    with open(dest_filename, "wb") as fp2:
-        # if this part is not an attachment, it is the body of the message, so
-        # we prepend some headers to give context
-        if not part.is_attachment() and part.get_content_maintype() == "text":
-            for h in ["Date", "From", "To", "Subject"]:
-                fp2.write(f"{h}: {msg[h.lower()]}\n".encode())
-            fp2.write("\n".encode())
+            fp2.write(part.get_payload(decode=True))
 
-        fp2.write(part.get_payload(decode=True))
+            logger.debug("saved %s", filename)
 
-        logger.debug("saved %s", filename)
+        # same as above, but if it was HTML and convert_html_to_pdf is on, convert
+        # the HTML to PDF and save that alongside, using prince
+        if ext == "html" and self.config.body.convert_html_to_pdf:
+            dest_pdffilename = dest_filename[: -len(ext)] + ".pdf"
+            subprocess.run(["prince", dest_filename, "-o", dest_pdffilename])
 
-    # same as above, but if it was HTML and convert_html_to_pdf is on, convert
-    # the HTML to PDF and save that alongside, using prince
-    if ext == "html" and config.body.convert_html_to_pdf:
-        dest_pdffilename = dest_filename[: -len(ext)] + ".pdf"
-        subprocess.run(["prince", dest_filename, "-o", dest_pdffilename])
+    def save_message(self, input_file, prompt_save_dir_command=None):
+        """Save the message in `input_file`, using rules to determine
+        where to save. If prompt_save_dir_command is specified, that is treated
+        as a command to run to determine the save location instead (generally it's
+        expected that this command would prompt the user for a choice, e.g. via `fzf`).
+        """
 
+        # create a temporary file and save incoming data to it; the file is
+        # opened with w+b, meaning write and read is possible, so we can then
+        # re-feed the file to the email parser, and then write it all to a new
+        # file in the output directory once we know what that directory is called
+        with tempfile.TemporaryFile(mode="w+") as fp:
+            for line in input_file:
+                fp.write(line)
 
-def save_message(input_file, prompt_save_dir_command=None):
-    """Save the message in `input_file`, using rules to determine
-    where to save. If prompt_save_dir_command is specified, that is treated
-    as a command to run to determine the save location instead (generally it's
-    expected that this command would prompt the user for a choice, e.g. via `fzf`).
-    """
+            fp.seek(0)
+            msg = email.message_from_file(fp, policy=email.policy.default)
+            # verbose_msg(msg)
 
-    # create a temporary file and save incoming data to it; the file is
-    # opened with w+b, meaning write and read is possible, so we can then
-    # re-feed the file to the email parser, and then write it all to a new
-    # file in the output directory once we know what that directory is called
-    with tempfile.TemporaryFile(mode="w+") as fp:
-        for line in input_file:
-            fp.write(line)
+            subject = sanitize_to_filename(msg["subject"])
 
-        fp.seek(0)
-        msg = email.message_from_file(fp, policy=email.policy.default)
-        verbose_msg(msg)
+            from_parts = email.utils.parseaddr(msg["from"])
+            date = datetime.strptime(msg["date"], "%a, %d %b %Y %H:%M:%S %z")
 
-        subject = sanitize_to_filename(msg["subject"])
+            name_from = from_parts[0] or from_parts[1]
+            name_date = date.strftime("%b%y")
+            message_name = f"{name_from} {subject} {name_date}"
 
-        from_parts = email.utils.parseaddr(msg["from"])
-        date = datetime.strptime(msg["date"], "%a, %d %b %Y %H:%M:%S %z")
+            matching_rules = match_save_rule_or_prompt(msg, prompt_save_dir_command)
+            logger.debug("matching_rules: %s", matching_rules)
+            rule = None
 
-        name_from = from_parts[0] or from_parts[1]
-        name_date = date.strftime("%b%y")
-        message_name = f"{name_from} {subject} {name_date}"
+            if len(matching_rules) > 0:
+                # find the first match with save_to specified
+                rule = list(filter(lambda x: x.save_to, matching_rules))[0]
 
-        matching_rules = match_save_rule_or_prompt(msg, prompt_save_dir_command)
-        logger.debug("matching_rules: %s", matching_rules)
-        rule = None
+                dest_dir = os.path.join(rule.save_to, message_name)
+            else:
+                dest_dir = os.path.join(
+                    self.config.default_save_to or DEFAULT_SAVE_TO, message_name
+                )
 
-        if len(matching_rules) > 0:
-            # find the first match with save_to specified
-            rule = list(filter(lambda x: x.save_to, matching_rules))[0]
+            dest_dir = os.path.expanduser(os.path.expandvars(dest_dir))
 
-            dest_dir = os.path.join(rule.save_to, message_name)
-        else:
-            dest_dir = os.path.join(
-                config.default_save_to or DEFAULT_SAVE_TO, message_name
-            )
+            logger.info("saving to %s", dest_dir)
+            os.makedirs(dest_dir, exist_ok=True)
 
-        dest_dir = os.path.expanduser(os.path.expandvars(dest_dir))
+            counter = 1
+            for part in msg.walk():
+                self.save_part(
+                    msg=msg,
+                    part=part,
+                    message_name=message_name,
+                    counter=counter,
+                    dest_dir=dest_dir,
+                )
 
-        logger.info("saving to %s", dest_dir)
-        os.makedirs(dest_dir, exist_ok=True)
-
-        counter = 1
-        for part in msg.walk():
-            save_part(
-                msg=msg,
-                part=part,
-                message_name=message_name,
-                counter=counter,
-                dest_dir=dest_dir,
-            )
-
-        # finally, re-read the raw input and write it to a file in the new directory
-        fp.seek(0)
-        message_file_name = f"{message_name}.eml"
-        with open(os.path.join(dest_dir, message_file_name), "w") as f:
-            for line in fp:
-                f.write(line)
-            logger.debug("saved %s", message_file_name)
+            # finally, re-read the raw input and write it to a file in the new directory
+            fp.seek(0)
+            message_file_name = f"{message_name}.eml"
+            with open(os.path.join(dest_dir, message_file_name), "w") as f:
+                for line in fp:
+                    f.write(line)
+                logger.debug("saved %s", message_file_name)
